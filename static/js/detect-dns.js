5c7274ab2a93a3b9b63ee43686eb0826a5b8da6b
Kristóf <55440418+libDarkstreet@users.noreply.github.com>
2022-04-30 16:35:28 +0200

    Update detect-dns.js

--- static/js/detect-dns.js
+++ static/js/detect-dns.js
@@ -1,5 +1,6 @@
 $.ajax({
     url: "https://32lk4tazjrpul4gw2fot.zerologdns.net/status?ch=" + Math.floor(Math.random() * 99999999),
+    cache: false,
     statusCode: {
         200: function() {
             document.getElementById('dns-test').innerHTML = "You are using ZeroLogDNS!🟢";
