234c89812abe165935ec133349870316c460f9e3
Kristóf <55440418+libDarkstreet@users.noreply.github.com>
2022-05-05 15:23:37 +0200

    Update preloader.js

--- static/js/preloader.js
+++ static/js/preloader.js
@@ -15,12 +15,8 @@ function enableScroll() {
     window.onscroll = function() {};
 }
 
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
-
 window.addEventListener("load", function(){
     disableScroll()
-    delay(2500).then(() => loader.style.display = "none");
-    delay(2300).then(() => enableScroll());
+    loader.style.display = "none";
+    enableScroll();
 })
