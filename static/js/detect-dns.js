0543bb8382e9f4267389e1596cad28cda986942d
Kristóf <55440418+libDarkstreet@users.noreply.github.com>
2022-07-25 22:35:07 +0200

    Update detect-dns.js

--- static/js/detect-dns.js
+++ static/js/detect-dns.js
@@ -1,13 +1,18 @@
 $.ajax({
-    url: "https://32lk4tazjrpul4gw2fot.zerologdns.net/status?ch=" + Math.floor(Math.random() * 99999999),
+    url: "https://t.zerologdns.net/?id=" + Math.floor(Math.random() * 99999999),
     cache: false,
-    statusCode: {
-        200: function() {
-            document.getElementById('dns-test').innerHTML = "You are using ZeroLogDNS! 🟢";
-
+    success(response) {
+        if (response.Response == "yes") {
+             document.getElementById('dns-test').innerHTML = "You are using ZeroLogDNS! 🟢";
+        } else if (response.Response == "no") {
+             document.getElementById('dns-test').innerHTML = "You are not using ZeroLogDNS! 🔴";
+        } else if (response.Response == "unavailable") {
+             document.getElementById('dns-test').innerHTML = "ZeroLogDNS back-end is not available!⚠️";
+        } else if (response.Response == "blocked") {
+             document.getElementById('dns-test').innerHTML = "The request was blocked from your network.💀";
         }
     },
     error:function(){
-        document.getElementById('dns-test').innerHTML = "You are not using ZeroLogDNS! 🔴";
-    }   
+        document.getElementById('dns-test').innerHTML = "Unable to connect!🔌";
+    }
 });
