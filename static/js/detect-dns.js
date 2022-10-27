9f5ffa4329a2e5af8488b7d36dff7bca762c0162
Kriss <55440418+libDarkstreet@users.noreply.github.com>
2022-10-27 15:37:55 +0200

    Update detect-dns.js

--- static/js/detect-dns.js
+++ static/js/detect-dns.js
@@ -4,12 +4,16 @@ $.ajax({
     success(response) {
         if (response.Response == "yes") {
              document.getElementById('dns-test').innerHTML = "You are using ZeroLogDNS! 🟢";
+             document.querySelector('title').textContent = "ZeroLogDNS - 🟢";
         } else if (response.Response == "no") {
              document.getElementById('dns-test').innerHTML = "You are not using ZeroLogDNS! 🔴";
+             document.querySelector('title').textContent = "ZeroLogDNS - 🔴";
         } else if (response.Response == "unavailable") {
              document.getElementById('dns-test').innerHTML = "ZeroLogDNS back-end is not available!⚠️";
+             document.querySelector('title').textContent = "ZeroLogDNS - ⚠️";
         } else if (response.Response == "blocked") {
              document.getElementById('dns-test').innerHTML = "The request was blocked from your network.💀";
+             document.querySelector('title').textContent = "ZeroLogDNS - 💀";
         }
     },
     error:function(){
